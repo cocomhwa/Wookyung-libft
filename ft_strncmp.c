a933bb3e69dae0925be354f774a3a78bf80f4993
cocomhwa <swk951031@gmail.com>
2022-07-07 09:27:21 +0900

    add : add strncmp.c
    add strncmp.c mark0. code from my piscine c03. just type of parameters
    are changed.

---
+++ ft_strncmp.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wooshin <wooshin@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/07 09:11:57 by wooshin           #+#    #+#             */
+/*   Updated: 2022/07/07 09:22:51 by wooshin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+	unsigned char	a;
+	unsigned char	b;
+
+	i = 0;
+	if (n < 1)
+		return (0);
+	while (i < n && (s1[i] != '\0' && s2[i] != '\0'))
+	{
+		if (s1[i] != s2[i])
+			break;
+		i++;
+	}
+	if (i == n)
+		return (0);
+	a = s1[i];
+	b = s2[i];
+	return (a - b);
+}
