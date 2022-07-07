7c1e784053553f5dd404f4d2e50639cf0f5bbbf2
Wookyung Shin <wooshin@c1r7s1.42seoul.kr>
2022-07-07 20:29:39 +0900

    add, fix : files fixed, add atoi.c
    atoi.c not finished.

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,14 +6,14 @@
 /*   By: wooshin <wooshin@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 09:11:57 by wooshin           #+#    #+#             */
-/*   Updated: 2022/07/07 09:22:51 by wooshin          ###   ########.fr       */
+/*   Updated: 2022/07/07 18:57:01 by wooshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t			i;
 	unsigned char	a;
 	unsigned char	b;
 
@@ -23,7 +23,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (i < n && (s1[i] != '\0' && s2[i] != '\0'))
 	{
 		if (s1[i] != s2[i])
-			break;
+			break ;
 		i++;
 	}
 	if (i == n)
--- libft.h
+++ libft.h
@@ -6,10 +6,9 @@
 /*   By: wooshin <wooshin@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 01:17:59 by wooshin           #+#    #+#             */
-/*   Updated: 2022/07/07 09:55:08 by wooshin          ###   ########.fr       */
+/*   Updated: 2022/07/07 18:58:42 by wooshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <stddef.h>
@@ -17,6 +16,6 @@
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcat(char *dst, const char *src, size_t destsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
