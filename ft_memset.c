3972b6ec3369f4f5fb73112178490144a3553fbd
cocomhwa <swk951031@gmail.com>
2022-07-10 23:21:34 +0900

    add : add ft_memset.c
    add amd slightly fix ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -6,12 +6,20 @@
 /*   By: wooshin <wooshin@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 23:16:30 by wooshin           #+#    #+#             */
-/*   Updated: 2022/07/09 23:18:09 by wooshin          ###   ########.fr       */
+/*   Updated: 2022/07/10 23:20:55 by wooshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	
+	size_t	i;
+
+	i = 0;
+	while (i < len)
+	{
+		*(unsigned char *)(b + i) = (unsigned char)c;
+		i++;
+	}
+	return (b);
 }
