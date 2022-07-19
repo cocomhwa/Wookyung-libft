c6bb3a2673b5f3bcf611844e11d3057ad75ded34
cocomhwa <swk951031@gmail.com>
2022-07-20 03:58:01 +0900

    add : add 2 files.

---
+++ ft_strjoin.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wooshin <wooshin@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/20 02:57:47 by wooshin           #+#    #+#             */
+/*   Updated: 2022/07/20 03:38:35 by wooshin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*res;
+	size_t	s1len;
+	size_t	s2len;
+
+	s1len = ft_strlen(s1);
+	s2len = ft_strlen(s2);
+	res = malloc(sizeof(char) * (s1len + s2len + 1));
+	if (!res)
+		return (0);
+	ft_strlcpy(res, s1, s1len + 1);
+	ft_strlcat(res + s1len, s2, s2len + 1);
+	return (res);
+}
--- ft_substr.c
+++ ft_substr.c
@@ -6,19 +6,20 @@
 /*   By: wooshin <wooshin@student.42seoul.k>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 15:48:15 by wooshin           #+#    #+#             */
-/*   Updated: 2022/07/13 17:03:09 by wooshin          ###   ########.fr       */
+/*   Updated: 2022/07/14 22:28:06 by wooshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	unsigned int	sidx;
-	char			*substring;	
+	size_t	sidx;
+	char	*substring;	
 
 	sidx = 0;
 	substring = malloc(sizeof(char) * (len + 1));
 	if (!substring)
 		return (0);
+	ft_strlcpy(substring, s, len + 1);
 	return (substring);
 }
