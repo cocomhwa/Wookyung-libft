9a83059b28af5933710221e92fa6e7f1e9d685ed
cocomhwa <swk951031@gmail.com>
2022-08-13 07:05:16 +0900

    fix file
    add func in libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: wooshin <wooshin@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 01:17:59 by wooshin           #+#    #+#             */
-/*   Updated: 2022/08/13 04:11:41 by wooshin          ###   ########.fr       */
+/*   Updated: 2022/08/13 07:04:50 by wooshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -44,5 +44,6 @@ char	*ft_substr(char	const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
 
 #endif
