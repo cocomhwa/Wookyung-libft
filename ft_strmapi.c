ce1872a4758222b3b7c376ba2c0925ee91c23b5d
cocomhwa <swk951031@gmail.com>
2022-08-13 07:24:36 +0900

    add file
    add ft_strmapi.c

---
+++ ft_strmapi.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wooshin <wooshin@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/13 07:07:10 by wooshin           #+#    #+#             */
+/*   Updated: 2022/08/13 07:22:08 by wooshin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*res;
+	size_t	i;
+
+	i = 0;
+	res = malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!res)
+		return (0);
+	while (s[i])
+	{
+		res[i] = f(i, s[i]);
+		i++;
+	}
+	res[i] = 0;
+	return (res);
+}
