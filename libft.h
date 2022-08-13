7e63e9b8e26ef104ddaf88412ffd821f8a3f6a50
cocomhwa <swk951031@gmail.com>
2022-08-13 09:15:43 +0900

    fix file
    add ft_strmapi.c in libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: wooshin <wooshin@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 01:17:59 by wooshin           #+#    #+#             */
-/*   Updated: 2022/08/13 07:04:50 by wooshin          ###   ########.fr       */
+/*   Updated: 2022/08/13 09:15:19 by wooshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -45,5 +45,6 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
 #endif
